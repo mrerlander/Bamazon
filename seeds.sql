3674c91f6bce509768229ad3b9135478fa19c76e
Michael@MIKE-LAPTOP <mrerlander@gmail.com>
2018-03-06 21:17:44 -0600

    created schema, seeds, and js files

---
+++ seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("headphones", "electronics", 75, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("vaccuum", "housewares", 300, 75);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("diamond earrings", "jewelry", 650, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("tortilla chips", "grocery", 2.50, 150);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("gum", "grocery", .75, 200);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("blender", "housewares", 80, 40);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("pearl necklace", "jewelry", 300, 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("projector", "electronics", 450, 15);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("pillow", "bedding", 45, 20);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("comforter", "bedding", 80, 11);
